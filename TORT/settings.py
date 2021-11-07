5c1bed8d7db1f7ff2e86a260e415b698ad37fac6
EsleinSlein <83123675+EsleinSlein@users.noreply.github.com>
2021-11-07 15:06:41 +0300

    Update settings.py

--- TORT/settings.py
+++ TORT/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1r#_pfws)il5vjp+b)+e^*a6a4&=74u@$en@&qf*dc*5ksy6q#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +79,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
          'NAME': 'ltr',
          'USER': 'ltruser',
-         'PASSWORD': '123456789',
+         'PASSWORD': '12',
          'HOST': 'localhost',
          'PORT': '',
      }
